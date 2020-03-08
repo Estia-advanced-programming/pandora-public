4c09d5d350972808dd1c5d76366ce9e13571995c
William Delamare <w.delamare@estia.fr>
2020-03-08 15:05:56 +0100

    Transform python  Issue script into a github action
    formated for action command line arguments

--- issueGenerator/Main/Main.py
+++ issueGenerator/Main/Main.py
@@ -7,20 +7,29 @@ Created on 6 Mar 2020
 
 import re
 import os
+import sys
+import getopt
 
-
+# https://pygithub.readthedocs.io/en/latest/introduction.html
 from github import Github
 from github.Milestone import Milestone
 from github.Label import Label
 
 from dotenv import load_dotenv
 
+'''
+To run the script outside Eclipse
+(otherwise import pbms)
+'''
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+os.chdir(SCRIPT_DIR)
+
 from Resources import Data as d
 
-load_dotenv("../Resources/.env")
+#load_dotenv("../Resources/.env")
 
-git_token = os.getenv("GIT_TOKEN")
-git_repo = "Estia-advanced-programming/pandora-template"
 
 file_feature = None
 dico_to_log = {}
@@ -29,13 +38,13 @@ output_json = "../TestFilesJSON/"
 
 
 def DisplayMDMilestones():
-    
+
     dico_nb = {}
     for issue in d.issues:
         if issue['milestone'] not in dico_nb:
             dico_nb[issue['milestone']] = 0
         dico_nb[issue['milestone']] += 1
-        
+
     s = ""
     i = 0
     for m in d.milestones:
@@ -43,54 +52,100 @@ def DisplayMDMilestones():
         s += "    * Number of issues: " + str(dico_nb[i]) + "\n"
         i += 1
     print(s)
-    
-    
-if __name__ == '__main__':
-    
-    DisplayMDMilestones()
-    exit(0)
-    
+
+
+def PrintHelp():
+    print('python Main.py -t <git_token> -r <git_repo> [-v]')
+    print("-t --token: mandatory (default: env variable)")
+    print("-r --repo: mandatory (default: 'Estia-advanced-programming/weekendtest-team3'")
+    print("v: verbose - optional (print Milestones in md format + test files json)")
+
+
+# make sure we have the correct links between d.milestones and milestones from github
+def GetMilestone(m_nb, milestones):
+    m_title = d.milestones[m_nb]['title']
+    for m in milestones:
+        if m.title.find(m_title) != -1:
+            return m
+    # nothing found?
+    print("A problem occured: could not find milestone " + m_nb)
+    sys.exit(1)
+
+
+# python Main.py -t <git_token> -r <git_repo> [-v]
+def main(argv):
+    git_token = os.getenv("INPUT_TOKEN")
+    git_repo = os.getenv("GITHUB_REPOSITORY")
+    verbose = False
+
+    try:
+        opts, args = getopt.getopt(argv,"ht:r:",["token=","repo="])
+    except getopt.GetoptError:
+        PrintHelp()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            PrintHelp()
+            sys.exit()
+        elif opt in ("-t", "--token"):
+            git_token = arg
+        elif opt in ("-r", "--repo"):
+            git_repo = arg
+        elif opt in ("-v",):
+            verbose = True
+
+    print( git_token )
+    print( git_repo )
+
     g = Github(git_token)
     repo = g.get_repo(git_repo)
-    
+
     for label in d.labels:
         try:
             repo.create_label(name = label["name"], color=label['color'], description="")
         except:
             pass
     labels = repo.get_labels()
-    
+
     for milestone in d.milestones:
         try:
-            repo.create_milestone(title = milestone['title'], state = milestone['state'], description = milestone['description'])   
+            repo.create_milestone(title = milestone['title'], state = milestone['state'], description = milestone['description'])
         except:
             pass
-    milestones = repo.get_milestones(state='open')
-    
-    g_issues = repo.get_issues()
+
+    gmilestones = repo.get_milestones(state='all')
+
+    g_issues = repo.get_issues(state='all')
+
     i_names = []
     for gi in g_issues:
         i_names.append(gi.title)
 
     for issue in d.issues:
         if issue['title'] not in i_names:
-            val = repo.create_issue(title = issue['title'], body = issue['body'], milestone=milestones[issue['milestone']], labels=issue['labels'])
+            val = repo.create_issue(title = issue['title'], body = issue['body'], milestone=GetMilestone(issue['milestone'], gmilestones), labels=issue['labels'])
             print("creating " + val.title)
-        
-        optionLine = ""
-        # "**CLI Output Name**: -o flightDuration\n\n"
-        tab = re.findall(re.compile("CLI Output Name.*(\-o .*?)\s\s", re.DOTALL), issue['body'])
-        if len(tab) > 0:
-            optionLine = tab[0]
-            if issue['milestone'] not in dico_to_log:
-                dico_to_log[issue['milestone']] = []
-            dico_to_log[issue['milestone']].append({"name" : issue['title'], "optionLine":optionLine, "testFile": d.milestones[issue['milestone']]['file']})
-            
-    for k,v in dico_to_log.items():
-        print("create file for milestone " + str(k))
-        f = open(output_json + "milestone_" + str(k), "w+")
-        f.write(str(v))
-        f.close()
-        
-        
-    DisplayMDMilestones()
+
+        if verbose:
+            optionLine = ""
+            # "**CLI Output Name**: -o flightDuration\n\n"
+            tab = re.findall(re.compile("CLI Output Name.*(\-o .*?)\s\s", re.DOTALL), issue['body'])
+            if len(tab) > 0:
+                optionLine = tab[0]
+                if issue['milestone'] not in dico_to_log:
+                    dico_to_log[issue['milestone']] = []
+                dico_to_log[issue['milestone']].append({"name" : issue['title'], "optionLine":optionLine, "testFile": d.milestones[issue['milestone']]['file']})
+
+    if verbose:
+        for k,v in dico_to_log.items():
+            print("create file for milestone " + str(k))
+            f = open(output_json + "milestone_" + str(k), "w+")
+            f.write(str(v))
+            f.close()
+
+    if verbose:
+        DisplayMDMilestones()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
