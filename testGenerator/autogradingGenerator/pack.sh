e6f51b195e945cfbd5ecb4d7c6e096b3c5894836
William Delamare <w.delamare@estia.fr>
2020-03-09 09:36:42 +0100

    Try to finally commit
    I don't know...

--- testGenerator/autogradingGenerator/pack.sh
+++ testGenerator/autogradingGenerator/pack.sh
@@ -1,3 +1,3 @@
-cd testSuite 
-seq 1 8 | xargs -R2 -I __i__ zip -r milestone__i__.zip all mono multi milestone__i__.json
+cd testSuite
+zip -r milestones.zip all mono multi milestone*.json
 mv *.zip ../../../testSuites/
